2a3cb6805ff701e16ae5f1ada6de1865749e7f99
Brandon Espinoza <espinozab100@gmail.com>
2015-06-15 19:20:43 -0500

    converted to index.php and included email contact form files | SmoothScroll installed

---
+++ index.php
@@ -0,0 +1,257 @@
+<?php include('includes/ewp.php');
+
+include('includes/mail_script.php');
+
+?>
+<html>
+  <head>
+    <title>NSS Cohort-8</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Nashville Software School Cohort 8</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#aboutUs">About Us</a></li>
+            <li><a href="#skills">Our Skills</a></li>
+            <li><a href="#whoRwe">Who Are We</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+    <div id="home" class="col-sm-12 col-lg-12">
+
+    </div>
+
+
+    <div id="aboutUs" class="col-sm-12 col-lg-12 ">
+      <div class="col-sm-10 col-sm-offset-1  ">
+        <img class="img-responsive proflie" src="img/nss-8_final_2_1.png">
+        <h1 class="abouth1">Nashville Software School Cohort-8</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+      </div>
+    </div>
+
+    <div id="skills" class="col-lg-12">
+      <div class="container">
+        <h1>Our Skills</h1><hr>
+        <div class="skillLogo col-sm-12 col-lg-12">
+          <div class=" col-lg-3">
+            <img class="img-responsive img-logos" src="img/HTML5.png">
+          </div>
+          <div class=" col-lg-3 ">
+            <img class="img-responsive img-logos" src="img/css3.png">
+          </div>
+          <div class=" col-lg-3 ">
+            <img class="img-responsive img-logos" src="img/JavaScript-logo.png">
+          </div>
+          <div class=" col-lg-3 ">
+            <img class="img-responsive img-logos" src="img/angularjs.png">
+          </div>
+          <div class=" col-lg-3 ">
+            <img class="img-responsive img-logos" src="img/jquery_logo.png">
+          </div>
+          <div class=" col-lg-3 ">
+            <img class="img-responsive img-logos" src="img/github.png">
+          </div>
+          <div class=" col-lg-3 ">
+            <img class="img-responsive img-logos" src="img/Ruby_logo.png">
+          </div>
+          <div class=" col-lg-3 ">
+            <img class="img-responsive img-logos" src="img/rails.png">
+          </div>
+          <div class=" col-lg-3 ">
+            <img class="img-responsive img-logos" src="img/sqlite.png">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div id="whoRwe" class="col-sm-12 ">
+      <div class="container">
+        <h1>Who Are We</h1><hr>
+        <div class="col-lg-12 classmates">
+          <div class="imgWrap one imgMargin col-sm-6 col-md-6 col-lg-6 ">
+          	<img class="imgs" src="img/NSS%20Cohort%208%20Final/Individuals/allisonBW.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Brandon Ray Howard</h1>
+              <p class="imgp">I love rock climbing :)</p>
+              <a target="_blank" href="htpp://www.github.com/bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-sm-offset-1 col-md-6 col-lg-6 col-lg-offset-1">
+          	<img class="imgs" src="img/portraits/profile_pic3.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Brandon Espinoza</h1>
+              <p class="imgp">Ping Pong Master</p>
+              <a target="_blank" href="https://github.com/Bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-md-6 col-lg-6 col-lg-offset-1 ">
+          	<img class="imgs" src="img/portraits/erinpage.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Erin M Page</h1>
+              <p class="imgp">Hacker Status</p>
+              <a target="_blank" href="https://github.com/Bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-md-6 col-lg-6 ">
+          	<img class="imgs" src="img/portraits/brandonrhoward.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Brandon Ray Howard</h1>
+              <p class="imgp">I love rock climbing :)</p>
+              <a target="_blank" href="htpp://www.github.com/bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-sm-offset-1 col-md-6 col-lg-6 col-lg-offset-1">
+          	<img class="imgs" src="img/portraits/profile_pic3.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Brandon Espinoza</h1>
+              <p class="imgp">Ping Pong Master</p>
+              <a target="_blank" href="https://github.com/Bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-md-6 col-lg-6 col-lg-offset-1 ">
+          	<img class="imgs" src="img/portraits/erinpage.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Erin M Page</h1>
+              <p class="imgp">Hacker Status</p>
+              <a target="_blank" href="https://github.com/Bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-md-6 col-lg-6 ">
+          	<img class="imgs" src="img/portraits/brandonrhoward.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Brandon Ray Howard</h1>
+              <p class="imgp">I love rock climbing :)</p>
+              <a target="_blank" href="htpp://www.github.com/bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-sm-offset-1 col-md-6 col-lg-6 col-lg-offset-1">
+          	<img class="imgs" src="img/portraits/profile_pic3.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Brandon Espinoza</h1>
+              <p class="imgp">Ping Pong Master</p>
+              <a target="_blank" href="https://github.com/Bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-md-6 col-lg-6 col-lg-offset-1 ">
+          	<img class="imgs" src="img/portraits/erinpage.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Erin M Page</h1>
+              <p class="imgp">Hacker Status</p>
+              <a target="_blank" href="https://github.com/Bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-md-6 col-lg-6 ">
+          	<img class="imgs" src="img/portraits/brandonrhoward.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Brandon Ray Howard</h1>
+              <p class="imgp">I love rock climbing :)</p>
+              <a target="_blank" href="htpp://www.github.com/bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-sm-offset-1 col-md-6 col-lg-6 col-lg-offset-1">
+          	<img class="imgs" src="img/portraits/profile_pic3.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Brandon Espinoza</h1>
+              <p class="imgp">Ping Pong Master</p>
+              <a target="_blank" href="https://github.com/Bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-md-6 col-lg-6 col-lg-offset-1 ">
+          	<img class="imgs" src="img/portraits/erinpage.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Erin M Page</h1>
+              <p class="imgp">Hacker Status</p>
+              <a target="_blank" href="https://github.com/Bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-md-6 col-lg-6 ">
+          	<img class="imgs" src="img/portraits/brandonrhoward.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Brandon Ray Howard</h1>
+              <p class="imgp">I love rock climbing :)</p>
+              <a target="_blank" href="htpp://www.github.com/bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-sm-offset-1 col-md-6 col-lg-6 col-lg-offset-1">
+          	<img class="imgs" src="img/portraits/profile_pic3.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Brandon Espinoza</h1>
+              <p class="imgp">Ping Pong Master</p>
+              <a target="_blank" href="https://github.com/Bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+          <div class="imgWrap one imgMargin col-sm-6 col-md-6 col-lg-6 col-lg-offset-1 ">
+          	<img class="imgs" src="img/portraits/erinpage.jpg" width="300" height="auto"/>
+            <div class="content">
+            	<h1 class="imgh1">Erin M Page</h1>
+              <p class="imgp">Hacker Status</p>
+              <a target="_blank" href="https://github.com/Bespinoza10"><i class="fa fa-github social"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/bespinoza10"><i class="dibujo fa fa-linkedin social"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- EMAIL CONTACT FORM  -->
+    <div id="contact" class="col-sm-12 col-md-12 col-lg-12 cont">
+      <div class="container">
+        <div class="row">
+          <form  action="" method="post" class="col-lg-8 col-lg-offset-2">
+            <h1 class="whtIdo h1s contactH1">Contact</h1><hr>
+            <div class="form-group cont">
+              <label class="contactHs">Full Name:</label>
+              <input type="text" name="Name" class="form-control" value="<?php get_data('Name'); ?>" placeholder="John Smith">
+            </div>
+            <div class="form-group">
+              <label class="contactHs">Email address</label>
+              <input type="email" name="Email" class="form-control" id="exampleInputEmail1" value="<?php get_data("Email"); ?>" placeholder="john@smith.com">
+            </div>
+            <label class="contactHs">Your Message</label>
+            <textarea class="form-control" rows="3" name="Comments" placeholder="Hello this is my message"><?php get_data("Comments"); ?></textarea>
+            <button type="submit" class="btn btn-default btn-lg contactHs">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+  <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/main.js"></script>
+  </body>
+</html>
